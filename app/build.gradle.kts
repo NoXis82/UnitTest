c4de27af86239e6779a1680498bd387a3c4bdc32
Tumanov Vycheslav <63115274+NoXis82@users.noreply.github.com>
2024-08-24 22:32:10 +0300

    add navigate and ui fragment's

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,7 +62,9 @@ android {
 dependencies {
     implementation ("androidx.appcompat:appcompat:1.7.0")
     implementation ("androidx.fragment:fragment-ktx:1.8.2")
-
+    implementation ("androidx.constraintlayout:constraintlayout:1.1.3")
+    // Material Design
+    implementation ("com.google.android.material:material:1.3.0-alpha02")
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
@@ -76,6 +78,8 @@ dependencies {
     //Room
     implementation(libs.room.runtime)
     implementation(libs.room.ktx)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
     kapt(libs.room.compiler)
 
     //Hilt
@@ -106,6 +110,9 @@ dependencies {
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
     androidTestImplementation(libs.truth)
+//    Mockito
+    androidTestImplementation(libs.mockito)
+    testImplementation(libs.mockito)
 
     debugImplementation("androidx.fragment:fragment-testing-manifest:1.8.2")
 

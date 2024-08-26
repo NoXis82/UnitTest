6e531b3fcc7e3e102518bb7a78b923f206ea4f50
Tumanov Vycheslav <63115274+NoXis82@users.noreply.github.com>
2024-08-26 22:30:09 +0300

    Test Swipe to Delete Functionality

--- app/src/androidTest/java/com/noxis/unittest/ui/ShoppingFragmentTest.kt
+++ app/src/androidTest/java/com/noxis/unittest/ui/ShoppingFragmentTest.kt
@@ -1,12 +1,19 @@
 package com.noxis.unittest.ui
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.swipeLeft
+import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.filters.MediumTest
+import com.google.common.truth.Truth.assertThat
 import com.noxis.unittest.R
+import com.noxis.unittest.adapters.ShoppingAdapter
+import com.noxis.unittest.data.local.ShoppingItem
+import com.noxis.unittest.getOrAwaitValue
 import com.noxis.unittest.launchFragmentInHiltContainer
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
@@ -16,6 +23,7 @@ import org.junit.Rule
 import org.junit.Test
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.verify
+import javax.inject.Inject
 
 @MediumTest
 @HiltAndroidTest
@@ -25,10 +33,37 @@ class ShoppingFragmentTest {
     @get:Rule
     var hiltAndroidRule = HiltAndroidRule(this)
 
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @Inject
+    lateinit var testShoppingFragmentFactory: TestShoppingFragmentFactory
+
     @Before
     fun setUp() {
         hiltAndroidRule.inject()
     }
+
+    @Test
+    fun swipeShoppingItem_deleteItemInDb() {
+        val shoppingItem = ShoppingItem("TEST", 1, 1f, "TEST", 1)
+        var testViewModel: ShoppingViewModel? = null
+        launchFragmentInHiltContainer<ShoppingFragment>(fragmentFactory = testShoppingFragmentFactory) {
+            testViewModel = viewModel
+            viewModel?.insertShoppingItemIntoDb(shoppingItem)
+        }
+
+        onView(withId(R.id.rvShoppingItems)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<ShoppingAdapter.ShoppingViewHolder>(
+                0,
+                swipeLeft()
+            )
+        )
+
+        assertThat(testViewModel?.shoppingItems?.getOrAwaitValue()).isEmpty()
+
+    }
+
 
     @Test
     fun clickAddShoppingItemButton_navigateToAddShoppingItemFragment() {
--- app/src/main/java/com/noxis/unittest/ui/ShoppingFragment.kt
+++ app/src/main/java/com/noxis/unittest/ui/ShoppingFragment.kt
@@ -3,23 +3,79 @@ package com.noxis.unittest.ui
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textview.MaterialTextView
 import com.noxis.unittest.R
+import com.noxis.unittest.adapters.ShoppingAdapter
+import javax.inject.Inject
 
 
-class ShoppingFragment : Fragment(R.layout.fragment_shopping) {
+class ShoppingFragment @Inject constructor(
+    val shoppingAdapter: ShoppingAdapter,
+    var viewModel: ShoppingViewModel? = null
+) : Fragment(R.layout.fragment_shopping) {
 
-    lateinit var viewModel: ShoppingViewModel
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewModel = ViewModelProvider(requireActivity())[ShoppingViewModel::class.java]
+        viewModel = viewModel ?: ViewModelProvider(requireActivity())[ShoppingViewModel::class.java]
+        subscribeToObservers(view)
+        setupRecyclerView(view)
+
         view.findViewById<FloatingActionButton>(R.id.fabAddShoppingItem).setOnClickListener {
             findNavController().navigate(
                 ShoppingFragmentDirections.actionShoppingFragmentToAddShoppingItemFragment()
             )
+        }
+    }
+
+    private val itemTouchCallback = object : ItemTouchHelper.SimpleCallback(
+        0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
+    ) {
+        override fun onMove(
+            recyclerView: RecyclerView,
+            viewHolder: RecyclerView.ViewHolder,
+            target: RecyclerView.ViewHolder
+        ): Boolean = true
+
+        override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+            val pos = viewHolder.layoutPosition
+            val item = shoppingAdapter.shoppingItems[pos]
+            viewModel?.deleteShoppingItem(item)
+//            Snackbar.make(requireView(), "Successfully deleted item", Snackbar.LENGTH_LONG).apply {
+//                setAction("Undo") {
+//                    viewModel?.insertShoppingItemIntoDb(item)
+//                }
+//                show()
+//            }
+
+        }
+
+    }
+
+    private fun subscribeToObservers(view: View) {
+        viewModel?.shoppingItems?.observe(viewLifecycleOwner, Observer {
+            shoppingAdapter.shoppingItems = it
+        })
+        viewModel?.totalPrice?.observe(viewLifecycleOwner, Observer {
+            val price = it ?: 0f
+            val priceText = "Total Price: $price€"
+            view.findViewById<MaterialTextView>(R.id.tvShoppingItemPrice).text = priceText
+        })
+    }
+
+    private fun setupRecyclerView(view: View) {
+        view.findViewById<RecyclerView>(R.id.rvShoppingItems).apply {
+            adapter = shoppingAdapter
+            layoutManager = LinearLayoutManager(requireContext())
+            ItemTouchHelper(itemTouchCallback).attachToRecyclerView(this)
         }
     }
 }

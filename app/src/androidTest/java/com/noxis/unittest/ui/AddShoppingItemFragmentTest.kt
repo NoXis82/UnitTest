0097962a2cd8440dd888b68ddfc929f040107013
Tumanov Vycheslav <63115274+NoXis82@users.noreply.github.com>
2024-08-26 21:23:57 +0300

    Test Addition of Shopping Items

--- app/src/androidTest/java/com/noxis/unittest/ui/AddShoppingItemFragmentTest.kt
+++ app/src/androidTest/java/com/noxis/unittest/ui/AddShoppingItemFragmentTest.kt
@@ -1,23 +1,30 @@
 package com.noxis.unittest.ui
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.Espresso.pressBack
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.replaceText
+import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.filters.MediumTest
+import com.google.common.truth.Truth.assertThat
 import com.noxis.unittest.R
+import com.noxis.unittest.data.local.ShoppingItem
+import com.noxis.unittest.getOrAwaitValue
 import com.noxis.unittest.launchFragmentInHiltContainer
+import com.noxis.unittest.repositories.FakeShoppingRepositoryAndroidTest
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.verify
+import javax.inject.Inject
 
 @MediumTest
 @HiltAndroidTest
@@ -25,12 +32,42 @@ import org.mockito.Mockito.verify
 class AddShoppingItemFragmentTest {
 
     @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @get:Rule
     var hiltAndroidRule = HiltAndroidRule(this)
+
+    @Inject
+    lateinit var fragmentFactory: ShoppingFragmentFactory
 
     @Before
     fun setUp() {
         hiltAndroidRule.inject()
     }
+
+    @Test
+    fun clickInsertIntoDb_shoppingItemInsertedIntoDb() {
+        val testViewmodel = ShoppingViewModel(FakeShoppingRepositoryAndroidTest())
+        launchFragmentInHiltContainer<AddShoppingItemFragment>(fragmentFactory = fragmentFactory) {
+            viewModel = testViewmodel
+        }
+
+        onView(withId(R.id.etShoppingItemName)).perform(replaceText("shopping item"))
+        onView(withId(R.id.etShoppingItemAmount)).perform(replaceText("5"))
+        onView(withId(R.id.etShoppingItemPrice)).perform(replaceText("3.4"))
+
+        onView(withId(R.id.btnAddShoppingItem)).perform(click())
+
+        assertThat(testViewmodel.shoppingItems.getOrAwaitValue()).contains(
+            ShoppingItem(
+                name = "shopping item",
+                amount = 5,
+                price = 3.4f,
+                imageUrl = ""
+            )
+        )
+    }
+
 
     @Test
     fun clickAddShoppingItemButton_navigateToAddShoppingItemFragment() {
--- app/src/main/java/com/noxis/unittest/ui/AddShoppingItemFragment.kt
+++ app/src/main/java/com/noxis/unittest/ui/AddShoppingItemFragment.kt
@@ -1,22 +1,41 @@
 package com.noxis.unittest.ui
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Button
 import android.widget.ImageView
 import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.RequestManager
+import com.google.android.material.textfield.TextInputEditText
 import com.noxis.unittest.R
+import com.noxis.unittest.other.Status
+import javax.inject.Inject
 
 
-class AddShoppingItemFragment : Fragment(R.layout.fragment_add_shopping_item) {
+class AddShoppingItemFragment @Inject constructor(
+    val glide: RequestManager
+) : Fragment(R.layout.fragment_add_shopping_item) {
 
     lateinit var viewModel: ShoppingViewModel
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProvider(requireActivity())[ShoppingViewModel::class.java]
+        subscribeToObserver(view)
+
+        view.findViewById<Button>(R.id.btnAddShoppingItem).setOnClickListener {
+            viewModel.insertShoppingItem(
+                view.findViewById<TextInputEditText>(R.id.etShoppingItemName).text.toString(),
+                view.findViewById<TextInputEditText>(R.id.etShoppingItemAmount).text.toString(),
+                view.findViewById<TextInputEditText>(R.id.etShoppingItemPrice).text.toString(),
+            )
+        }
+
 
         view.findViewById<ImageView>(R.id.ivShoppingImage).setOnClickListener {
             findNavController().navigate(
@@ -32,5 +51,29 @@ class AddShoppingItemFragment : Fragment(R.layout.fragment_add_shopping_item) {
         }
 
         requireActivity().onBackPressedDispatcher.addCallback(callback)
+    }
+
+    private fun subscribeToObserver(view: View) {
+        viewModel.curImageUrl.observe(viewLifecycleOwner, Observer {
+            glide.load(it).into(view.findViewById(R.id.ivShoppingImage))
+        })
+
+        viewModel.insertShoppingItemStatus.observe(viewLifecycleOwner, Observer {
+            it.getContentIfNotHandled()?.let { result ->
+                when (result.status) {
+                    Status.SUCCESS -> {
+                        Log.d("TAG", "subscribeToObserver: SUCCESS")
+                    }
+
+                    Status.ERROR -> {
+                        Log.d("TAG", "subscribeToObserver: ERROR")
+                    }
+
+                    Status.LOADING -> {
+                        Log.d("TAG", "subscribeToObserver: LOADING")
+                    }
+                }
+            }
+        })
     }
 }
